ae5a807fb354d00a700e272897aedb1afecedd31
ojasvi singhal <osinghal00@gmail.com>
2016-07-08 00:18:29 +0530

    Add files via upload

--- home.py
+++ home.py
@@ -1,66 +1,103 @@
+
 #!/usr/bin/python2
 
-import servers,os,commands,socket
+import services,os,signal,commands,socket,time,sys
+s = socket.socket()
 
-s=socket.socket()
-ip=""
-port=3000
-s.bind((ip,port))
-s.listen(5)
+s.connect(("192.168.122.1",3000))
 
+os.system("dialog --inputbox 'Enter username' 7 60 2>/tmp/user.txt")
+fh = open("/tmp/user.txt")
+user = fh.read()
+fh.close()
 
-def main(c):
-	username = c.recv(40)
-	password = c.recv(40)
+os.system("dialog --insecure --passwordbox 'Enter password' 7 60 2>/tmp/passwd.txt")
+fk = open("/tmp/passwd.txt")
+passwd = fk.read()
+fk.close()
 
-	#recieving signal whether to login ar register
-	sign = c.recv(30)
-	#login
-	if sign == '1':
-		y= commands.getstatusoutput("cd /home/{}".format(username))
-		if y[0]==0:
-			#checking password
-			z=commands.getstatusoutput("sshpass -p {} ssh {}@192.168.122.89 ".format(password,username))
-			c.send("1")
-			data = c.recv(30)
-		        if data == '1':
-		                servers.staas(c,username,password)
-		        elif data == '2':
-		                servers.iaas(c,username,password)
-		        elif data == '3':
-		                servers.paas(c,username,password)
-		        elif data == '4':
-		                servers.saas(c,username,password)
+s.send(user)
+s.send(passwd)
 
-		else:
-		#send signal that user does not exits
-			c.send("0")
+os.system("dialog --radiolist 'Login / Registration' 15 60 2 1 'Login' on 2 'register' off  2>/tmp/rgis.txt")
+fk = open("/tmp/rgis.txt")
+sign = fk.read()
+fk.close()
+
+#sending signal whether to login ar register
+s.send(sign)
+if sign == '1':
+#recieving signal
+	sk = s.recv(30)
+	if sk == '1':
+		os.system("dialog --radiolist   'Choose what you want , We will provide !!!' 15 60 6 1 'Storage as a Service(STAAS)'   on     2 'Infrastructure as a Service(IAAS)'  off    3 'Platform as a Service(PAAS)'  off    4 'Software as a Service(SAAS)'     off  5 'Container as a Service(CAAS)' off 6 'EXIT' off    2>/tmp/a.txt")
 
 
-	#register
+	        fh = open("/tmp/a.txt")
+	        data = fh.read()
+	        fh.close()
+
+        	s.send(data)
+	        if data == '1':
+        	        services.staas(s,user,passwd)
+
+        	elif data == '2':
+                	services.iaas(s,user,passwd)
+
+        	elif data == '3':
+                	services.paas(s,user,passwd)
+
+        	elif data == '4':
+                	services.saas(s,user,passwd)
+
+		elif data == '5':
+			services.caas(s,user,passwd)
+        	else:
+                	os.system("dialog --infobox 'Bye ! Have a nice day .....' 15 30")
+                	time.sleep(2)
+                	s.close()
+
+		
 	else:
-		y= commands.getstatusoutput("cd /home/{}".format(username))
-		if y[0]==0:
-			#sending signal user already exists
-			c.send("0")
+		os.system("dialog --infobox 'No Username or password exist' 5 50")
+
+#register
+
+elif sign == '2':
+#recieving signal
+	sk = s.recv(30)
+	if sk == '1':
+		os.system("dialog --infobox 'Registration Successful' 5 80")
+		os.system("dialog --radiolist   'Choose what you want , We will provide !!!' 15 60 6 1 'Storage as a Service(STAAS)'   on     2 'Infrastructure as a Service(IAAS)'  off    3 'Platform as a Service(PAAS)'  off    4 'Software as a Service(SAAS)'     off  5 'Container as a Service(CAAS)' off 6 'EXIT' off    2>/tmp/a.txt")
+
+		fh = open("/tmp/a.txt")
+		data = fh.read()
+		fh.close()
+
+		s.send(data)
+		if data == '1':
+			services.staas(s,user,passwd)
+
+		elif data == '2':
+			services.iaas(s,user,passwd)
+
+		elif data == '3':
+			services.paas(s,user,passwd)
+
+		elif data == '4':
+			services.saas(s,user,passwd)
+
+		elif data == '5':
+			services.caas(s,user,passwd)
+
 		else:
-			os.system("useradd {}".format(username))
-			os.system("echo {}| passwd {} --stdin".format(password,username))
-	#sending signal for successful registration
-			c.send("1")
+			os.system("dialog --infobox 'Bye ! Have a nice day .....' 15 30")
+			time.sleep(2)
+			s.close()
 
-			data = c.recv(30)
-			if data == '1':
-				servers.staas(c,username,password)
-			elif data == '2':
-				servers.iaas(c,username,password)
-			elif data == '3':
-				servers.paas(c,username,password)
-			elif data == '4':
-				servers.saas(c,username,password)
+	else:
+		os.system("dialog --infobox 'Oops!! user with this username already exists' 7 70")
 
 
 
-while True:
-	c,addr=s.accept()
-	main(c)
+
--- services.py
+++ services.py
@@ -1,17 +1,30 @@
-import os,commands
+#!/usr/bin/python2
+import os,commands,time
+
+
+################################################ STAAS ########################################################
+
+
 
 def staas(c,username,password):
-	os.system("dialog --radiolist 'choose' 20 40 3 1 'Object storage' on 2 'Block storage' off 3 'BACK' off 2>/tmp/b.txt")
+	os.system("dialog --radiolist 'Choose your Storage' 8 60 3 1 'Object storage' on 2 'Block storage' off 3 'EXIT' off 2>/tmp/b.txt")
+
+
 	fh = open("/tmp/b.txt")
 	choice = fh.read()
 	fh.close()
+
 	#sending choice to server to start requested storage
 	c.send(choice)		
+############# object #########################################################################################
 	if choice == '1':
-		os.system("dialog --radiolist 'choose' 20 60 5 1 'NFS' on 2 'SSHFS' off 3 'SAMBA' off 4 'EXTEND' off  5 'REMOVE' off 2>/tmp/cd.txt")
+
+
+		os.system("dialog --radiolist 'Storage Services' 20 60 5 1 'NFS' on 2 'SSHFS' off 3 'SAMBA' off 4 'EXTEND' off  5 'REMOVE' off 2>/tmp/cd.txt")
 		gh = open("/tmp/cd.txt")
 		dat = gh.read()
 		gh.close()
+
 		# sending which type of server user want
 		c.send(dat)
 		"""
@@ -25,7 +38,7 @@ def staas(c,username,password):
 		"""
 #nfs
 		if dat == '1':
-			os.system("dialog --inputbox 'Enter harddisk size' 20 60 2>/tmp/size.txt")
+			os.system("dialog --inputbox 'Enter harddisk size' 10 60 2>/tmp/size.txt")
 			g = open("/tmp/size.txt")
 			size = g.read()
 			g.close()
@@ -39,17 +52,11 @@ def staas(c,username,password):
 			# client side processing started
 			if signal == '1':
 				os.system("mkdir /root/Desktop/{}".format(username))
-				os.system("mount 192.168.122.1:/home/{0}/{0} /root/Desktop/{0}".format(username))
-				#doing entry to file after adding a partition using file handling
-				#os.system("echo '/home/{0}/nfs' >>/media/{0}.txt".format(username))
-				fi = open('/media/hdd.txt','w+')
-				fi.write("/home/{0}/{0}".format(username))
-				fi.close()
-			# done
-				#c.send("2")
-				os.system("dialog --infobox 'All Done enjoy!!!' 10 50")
+				os.system("mount 192.168.122.1:/home/nfs/{0} /root/Desktop/{0}".format(username))
+				
+				os.system("dialog --infobox 'All Done enjoy!!! Check ' 10 50")
 			else:
-				os.system("dialog --infobox 'some error occured'10 80")
+				os.system("dialog --infobox 'some error occured' 10 80")
 #sshfs server
 		elif dat == '2':
 			os.system("dialog --inputbox 'Enter harddisk size' 20 60 2>/tmp/size.txt")
@@ -69,10 +76,13 @@ def staas(c,username,password):
 				if y[0]!=0:	
 					os.system("yum install fuse-sshfs")
 				os.system("mkdir /root/Desktop/{}".format(username))
-				os.system("sshfs {0}@192.168.122.1:/home/{0}/{0} /root/Desktop/{0}".format(username))	
+				os.system("sshfs {0}@192.168.122.1:/home/sshfs/{0} /root/Desktop/{0}".format(username))	
 				os.system("dialog --infobox 'DONE !!check your Desktop to get access' 10 30")
 			else:
 				os.system("dialog --infobox 'Error Occured!!!! ' 20 30 ")
+
+
+
 
 #samba server
 		elif dat == '3':
@@ -93,7 +103,7 @@ def staas(c,username,password):
                                 if y[0]!=0:
                                         os.system("yum install cifs-utils samba-client -y")
 				os.system("mkdir /root/Desktop/{}".format(username))
-				os.system("mount -o username={0} //192.168.122.1/{0} /root/Desktop/{0}".format(username))
+				os.system("mount -o username={0}smb //192.168.122.1/{0} /root/Desktop/{0}".format(username))
 				os.system("dialog --infobox 'DONE !! ;) Hehe' 10 30")
 
 
@@ -103,14 +113,117 @@ def staas(c,username,password):
 
 #extend code
 		elif dat == '4':
-                        # checking database whether any drive exist with username
-                        # writing all existing lvs in a file
-			os.system("dialog --inputbox 'Enter name of hardisk' 30 40 2>/tmp/ex.txt")
+                        #recieving all name of disks
+			a1 = c.recv(60)
+			print a1
+			a2 = c.recv(60)
+			print a2
+			a3 = c.recv(60)
+			print a3
+			a4 = c.recv(60)
+			print a4
+
+			
+			f1 = a1.strip()
+			f2 = a2.strip()
+			f3 = a3.strip()
+			f4 = a4.strip()
+			print f1
+			print f2
+			print f3
+			print f4
+			
+			os.system("dialog --radiolist 'Choose hardisk to Extend' 16 50 4 1 {} off 2 {} off 3 {} off 4 {} off 2>/tmp/ex.txt".format(f1,f2,f3,f4))
 			gh = open("/tmp/size.txt")
 			name = gh.read()
 			gh.close()
-			#sending name of HDD to server
-			c.send(name)
+			
+			if name == '1' and a1 != 'None':
+				os.system("dialog --inputbox 'Enter size to Extend' 10 30 2>/tmp/size.txt")
+	                	g = open("/tmp/size.txt")
+	                	size = g.read()
+	                	g.close()
+				# sending type
+				c.send("nfs") 
+				# sending size to server
+	                	c.send(size)
+
+				#getting signal that server side processing is done
+	                	signal = c.recv(20)
+
+				if signal == '1':
+		                # done
+	                        	os.system("dialog --infobox 'Size Extended successfully' 10 50")
+	                	else:
+	                        	os.system("dialog --infobox 'some error occured' 10 80")
+
+			
+			elif name == '2' and a2 != 'None':
+				os.system("dialog --inputbox 'Enter size to Extend' 10 30 2>/tmp/size.txt")
+	                	g = open("/tmp/size.txt")
+	                	size = g.read()
+	                	g.close()
+				# sending type
+				c.send("sshfs") 
+				# sending size to server
+	                	c.send(size)
+
+				#getting signal that server side processing is done
+	                	signal = c.recv(20)
+
+				if signal == '1':
+		                # done
+	                        	os.system("dialog --infobox 'Size Extended successfully' 10 50")
+	                	else:
+	                        	os.system("dialog --infobox 'some error occured' 10 80")
+
+
+			elif name == '3' and a3 != 'None':
+				os.system("dialog --inputbox 'Enter size to Extend' 10 30 2>/tmp/size.txt")
+	                	g = open("/tmp/size.txt")
+	                	size = g.read()
+	                	g.close()
+				# sending type
+				c.send("iscsi") 
+				# sending size to server
+	                	c.send(size)
+
+				#getting signal that server side processing is done
+	                	signal = c.recv(20)
+
+				if signal == '1':
+		                # done
+	                        	os.system("dialog --infobox 'Size Extended successfully' 10 50")
+	                	else:
+	                        	os.system("dialog --infobox 'some error occured' 10 80")
+
+			elif name == '4' and a4 != 'None':
+				os.system("dialog --inputbox 'Enter size to Extend' 10 30 2>/tmp/size.txt")
+	                	g = open("/tmp/size.txt")
+	                	size = g.read()
+	                	g.close()
+				# sending type
+				c.send("samba") 
+				# sending size to server
+	                	c.send(size)
+
+				#getting signal that server side processing is done
+	                	signal = c.recv(20)
+
+				if signal == '1':
+		                # done
+	                        	os.system("dialog --infobox 'Size Extended successfully' 10 50")
+	                	else:
+	                        	os.system("dialog --infobox 'some error occured' 10 80")
+
+			else:
+				os.system('dialog --infobox "choose Valid disk" 5 40')
+
+
+
+				"""
+#sending name of HDD to server
+			#c.send(name)
 				#recieving signal from server
 			signals = c.recv(30)
 			if signals == '1':
@@ -133,7 +246,7 @@ def staas(c,username,password):
 	                        	os.system("dialog --infobox 'some error occured'10 80")
 			else:
 				os.system("dialog --infobox 'EROOR @@@@@@@@@@@@@@' 10 60")
-
+				"""
 	
 #remove code
 		elif dat == '5':
@@ -141,12 +254,10 @@ def staas(c,username,password):
 
 
 
-		else:
-			print 'xzzz'
-
+###########  block ###########################################################################################
 #block storage
 	elif choice == '2':
-		os.system("dialog --radiolist 'choose among these' 40 60 2 1 'ISCSI' on 2 'SNAPSHOT' off 2>/tmp/block.txt")
+		os.system("dialog --radiolist 'choose among these' 15 60 2 1 'ISCSI' on 2 'SNAPSHOT' off 2>/tmp/block.txt")
 
 		fd = open("/tmp/block.txt")
        		dat = fd.read()
@@ -155,8 +266,7 @@ def staas(c,username,password):
 		c.send(dat)
 #iscsi
 		if dat == '1':
-			os.system("dialog --inputbox 'Enter harddisk size' 20 60 2>/tmp/size.txt")
-                        k = open("/tmp/size.txt")
+			k = open("/tmp/size.txt")
                         size = k.read()
                         k.close()
 
@@ -171,9 +281,8 @@ def staas(c,username,password):
                                 y = commands.getstatusoutput("rpm -q iscsi-initiator-utils")
                                 if y[0]!=0:
                                         os.system("yum install iscsi-initiator-utils -y")
-				os.system("yum install iscsi-initiator-utils")
 				os.system("iscsiadm --mode discoverydb --type sendtargets --portal 192.168.122.1 --discover")
-				os.system("iscsiadm --mode node --targetname {} --portal 192.168.122.1:3260 --login".format(username))
+				os.system("iscsiadm --mode node --targetname {0} --portal 192.168.122.1:3260 --login".format(username))
 				os.system("dialog --infobox 'ALL DONE !! Check your Desktop' 10 30")	
 			else:
 				os.system("dialog --infobx 'Error Occured @@@@@@@@@@' 10 60")
@@ -182,18 +291,21 @@ def staas(c,username,password):
 			os.system("mkdir /root/Desktop/{}snap".format(username))
 			os.system("mount 192.168.122.1:/media/{0}snap /root/Desktop/{0}snap".format(username))
 
-#back option
+
+
+######## EXIT ###############################################################################################
+
+
+
+#exit option
 
 	elif choice == '3':
-		pass
-	
-	else:
-		pass
+		os.system("BBye !! We will miss you .......")
 	
 #staas completed
-
+###############################################  IAAS  ####################################################
 def iaas(c,username,password):
-	os.system("dialog --radiolist 'what you want to see' 20 30 2 1 'gallery' on 2 'install' off 2>/tmp/qq.txt")
+	os.system("dialog --radiolist 'OSREMOTE TOOL' 20 30 2 1 'OS GALLERY' on 2 'INSTALL OS' off 2>/tmp/qq.txt")
 	f = open("/tmp/qq.txt")
         choice = f.read()
         f.close()
@@ -201,38 +313,46 @@ def iaas(c,username,password):
 	#sending choice 
 	c.send(choice)
 
+
+
+
+############ OS GALLERY ########################################
 	if choice == '1':
 		sig =c.recv(30)
 		if sig == '0':
 			os.system("dialog --infobox 'no os yet' 10 30")
 
 		else:
-			p = c.recv(30) #recieving 1
+			#p = c.recv(30) #recieving 1
 			p1 = c.recv(20)
 			p2 = c.recv(20)
 			p3 = c.recv(20)
 
+
+			print p1
+			print p2
+			print p3
 			if p1 == '0':
 				port1 = c.recv(30)	
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port1))
-				os.system("firefox 192.168.122.1/vnc/vnc.html")
+				os.system("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port1))
 				
 			if p2 == '0':
 				port2 = c.recv(30)	
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port2))
-				os.system("firefox 192.168.122.1/vnc/vnc.html")
+				os.system("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port2))
 				
 
 			if p3 == '0':
 				port3 = c.recv(30)	
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port3))
-				os.system("firefox 192.168.122.1/vnc/vnc.html")
+				os.system("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port3))
 				
 
-
+########### INSTALL OS ###################################################
 	if choice == '2':		
 
-		os.system("dialog --radiolist 'choose your image' 20 30 3 1 'UBUNTU' on 2 'REDHAT 7.2' off  3 'WINDOWS' off 2>/tmp/image.txt")
+		os.system("dialog --radiolist 'ISO IMAGES' 20 30 3 1 'UBUNTU' on 2 'REDHAT 7.2' off  3 'WINDOWS' off 2>/tmp/image.txt")
 		fh = open("/tmp/image.txt")
 		image = fh.read()
 		fh.close()
@@ -240,9 +360,9 @@ def iaas(c,username,password):
 		#send signal
 		c.send(image)
 
-		os.system("dialog --inputbox 'enter ram' 4 60 2>/tmp/ram.txt")
-		os.system("dialog --inputbox 'enter cpu' 4 60 2>/tmp/cpu.txt")
-		os.system("dialog --inputbox 'enter harddisk' 4 60 2>/tmp/hd.txt")
+		os.system("dialog --inputbox 'enter ram' 7 30 2>/tmp/ram.txt")
+		os.system("dialog --inputbox 'enter cpu' 7 30 2>/tmp/cpu.txt")
+		os.system("dialog --inputbox 'enter harddisk' 7 30 2>/tmp/hd.txt")
 		a=open("/tmp/ram.txt")
 		ram = a.read()
 		a.close()
@@ -258,7 +378,7 @@ def iaas(c,username,password):
 		#sending all three values to server
 		c.send(ram)
 		c.send(cpu)
-		c.send(hd)
+		#c.send(hd)
 		if image == '1':
 			#receiving port where to connect
 			port = c.recv(50)
@@ -267,7 +387,7 @@ def iaas(c,username,password):
 			
 			if signal == '1':
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port))
-				commands.getstatusoutput("firefox 192.168.122.1/vnc/vnc.html")
+				commands.getstatusoutput("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port))
 			
 
 		elif image == '2':
@@ -278,7 +398,7 @@ def iaas(c,username,password):
 			
 			if signal == '1':
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port))
-				commands.getstatusoutput("firefox 192.168.122.1/vnc/vnc.html")
+				commands.getstatusoutput("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port))
 			
 		elif image == '3':
 			#receiving port where to connect
@@ -288,14 +408,14 @@ def iaas(c,username,password):
 			
 			if signal == '1':
 				os.system("dialog --infobox 'Port no is {}' 10 30 ".format(port))
-				commands.getstatusoutput("firefox 192.168.122.1/vnc/vnc.html")
+				commands.getstatusoutput("firefox http://192.168.122.1/vnc/?ip=192.168.122.1&port={}".format(port))
 			
 
 
 #iaas completed
-
+########################################################## PAAS #############################################
 def paas(c,username,password):
-	os.system("dialog --radiolist 'choose your platform' 30 80 3 1 'PYTHON' on 2 'EDITOR' off 3 'PHP' off 2>/tmp/sas.txt")
+	os.system("dialog --radiolist 'choose your platform' 30 80 3 1 'PYTHON' on 2 'MySQl' off 3 'PHP' off 2>/tmp/sas.txt")
         o = open("/tmp/sas.txt")
 	choice = o.read()
         o.close()
@@ -324,10 +444,10 @@ def paas(c,username,password):
 
 	
 #paas completed
-
+###################################################### CAAS ###############################################
 
 def caas(c,username,password):
-	os.system("dialog --radiolist 'choose your container' 30 80 3 1 'Centos' on 2 'Ubuntu' off 3 'Mint' off 2>/tmp/sas.txt")
+	os.system("dialog --radiolist 'Container as a Service' 30 80 3 1 'Centos' on 2 'Ubuntu' off 3 'MintOS' off 2>/tmp/sas.txt")
         o = open("/tmp/sas.txt")
 	choice = o.read()
         o.close()
@@ -353,10 +473,10 @@ def caas(c,username,password):
 
 
 
-
+################################################### SAAS ####################################################
 
 def saas(c,username,password):
-	os.system("dialog --radiolist 'choose your software' 30 80 3 1 'FIREFOX' on 2 'GEDIT' off 3 'VLC' off 2>/tmp/sas.txt")
+	os.system("dialog --radiolist 'Software as a service' 30 80 3 1 'FIREFOX' on 2 'GEDIT' off 3 'VLC' off 2>/tmp/sas.txt")
 	o = open("/tmp/sas.txt")
 	choice = o.read()
 	o.close()
@@ -364,12 +484,8 @@ def saas(c,username,password):
 	#sending choice to server to get permission
 	c.send(choice)
 	if choice == '1':
-		"""
-		os.system("dialog --inputbox 'Enter firefox accounts password' 20 80 2>/tmp/pswd.txt")
-		d = open("/tmp/pswd.txt")
-		pswd = d.read()
-		d.close()
-		"""
+
+#firefox
 		#setting signal to server to set password and create account for this user
 		data = c.recv(30)
 		if data == '1':
@@ -400,8 +516,10 @@ def saas(c,username,password):
                 if data == 1:
                 #sending password to server
                 #c.send(pswd)
-                        os.system("sshpass -p {} ssh -X {}@192.168.122.4".format(password,username))
+                        os.system("ssh -X {}@192.168.122.4".format(password,username))
                 else:
                         os.system("###$$$$$$$ ERROR VLC $$$$########")
 
 #saas completed
+###########################################################################################################
+ 
